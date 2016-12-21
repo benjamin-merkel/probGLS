98a8b65236fd1e5718c8d474e0311865a09fba2e
benjamin-merkel <benjamin.merkel@npolar.no>
2016-12-21 11:25:15 +0100

    changed string cut method

--- R/lotek_to_dataframe.R
+++ R/lotek_to_dataframe.R
@@ -8,6 +8,7 @@
 #' @param TRLat threshold method calculated latitude in daylog file
 #' @return A data.frame for further use in GeoLight or probGLS
 #' @return modified after trnTrans() in GeoLight by Tamara Emmenegger
+#' @import stringr
 #' @export
 
 lotek_to_dataframe<-function(date,sunrise,sunset,TRLon,TRLat){
@@ -26,11 +27,18 @@ lotek_to_dataframe<-function(date,sunrise,sunset,TRLon,TRLat){
   data$sunrise[data$sunrise==""] <- "12:00"
   data$sunset [data$sunset ==""] <- "12:00"
   
-  data$sr.H <- as.numeric(matrix(unlist(strsplit(as.character(data$sunrise),':')),ncol=2,byrow=T)[,1])
-  data$sr.M <- as.numeric(matrix(unlist(strsplit(as.character(data$sunrise),':')),ncol=2,byrow=T)[,2])
+  data$sr.H <- as.numeric(str_split_fixed(data$sunrise,":",2)[,1])
+  data$sr.M <- as.numeric(str_split_fixed(data$sunrise,":",2)[,2])
   
-  data$ss.H <- as.numeric(matrix(unlist(strsplit(as.character(data$sunset),':')),ncol=2,byrow=T)[,1])
-  data$ss.M <- as.numeric(matrix(unlist(strsplit(as.character(data$sunset),':')),ncol=2,byrow=T)[,2])
+  data$ss.H <- as.numeric(str_split_fixed(data$sunset,":",2)[,1])
+  data$ss.M <- as.numeric(str_split_fixed(data$sunset,":",2)[,2])
+  
+  
+#   data$sr.H <- as.numeric(matrix(unlist(strsplit(as.character(data$sunrise),':')),ncol=2,byrow=T)[,1])
+#   data$sr.M <- as.numeric(matrix(unlist(strsplit(as.character(data$sunrise),':')),ncol=2,byrow=T)[,2])
+#   
+#   data$ss.H <- as.numeric(matrix(unlist(strsplit(as.character(data$sunset),':')),ncol=2,byrow=T)[,1])
+#   data$ss.M <- as.numeric(matrix(unlist(strsplit(as.character(data$sunset),':')),ncol=2,byrow=T)[,2])
   
   data$sr <- data$date
   data$ss <- data$date
