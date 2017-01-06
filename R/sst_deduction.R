8e95d1c9f4ee7761e2de3cd780b3676a20c6f551
benjamin-merkel <benjamin.merkel@npolar.no>
2017-01-06 08:50:42 +0100

    return NULL if empty data is entered

--- R/sst_deduction.R
+++ R/sst_deduction.R
@@ -24,25 +24,29 @@ sst_deduction <- function(datetime,temp,temp.range=c(-2,30)){
   SST <- SST.remove <- NULL
   
   data       <- data.frame(datetime,temp)
-  data$dtime <- as.POSIXct(data$datetime,tz="UTC")
-  data$date  <- as.Date(data$datetime)
   
-  data$temp[data$temp > temp.range[2]]<-NA
-  data$temp[data$temp < temp.range[1]]<-NA
-  
-  temp           <- data.frame(tapply(data$temp,data$date,median,na.rm=T))
-  colnames(temp) <- "SST"
-  temp           <- cbind(date=as.Date(rownames(temp)),temp)
-  
-  temp$sst.step.before  <- temp$SST-c(NA,temp$SST[1:(nrow(temp)-1)])
-  temp$sst.step.after   <- temp$SST-c(temp$SST[2:(nrow(temp))],NA)
-  temp$sst.diff         <- temp$sst.step.before*temp$sst.step.after
-  #temp$wetdry.prob      <- 1-temp$wetdry_ratio/24
-  temp$sst.diff2        <- temp$sst.diff#*(temp$wetdry.prob+1)
-  temp$SST.remove       <- F
-  
-  temp$SST.remove[temp$sst.diff2>12] <- T
-  
-  output <- subset(temp,select=c(date,SST,SST.remove))
-  
+  if(nrow(data)==0){
+    output <- NULL
+  } else {
+    data$dtime <- as.POSIXct(data$datetime,tz="UTC")
+    data$date  <- as.Date(data$datetime)
+    
+    data$temp[data$temp > temp.range[2]]<-NA
+    data$temp[data$temp < temp.range[1]]<-NA
+    
+    temp           <- data.frame(tapply(data$temp,data$date,median,na.rm=T))
+    colnames(temp) <- "SST"
+    temp           <- cbind(date=as.Date(rownames(temp)),temp)
+    
+    temp$sst.step.before  <- temp$SST-c(NA,temp$SST[1:(nrow(temp)-1)])
+    temp$sst.step.after   <- temp$SST-c(temp$SST[2:(nrow(temp))],NA)
+    temp$sst.diff         <- temp$sst.step.before*temp$sst.step.after
+    #temp$wetdry.prob      <- 1-temp$wetdry_ratio/24
+    temp$sst.diff2        <- temp$sst.diff#*(temp$wetdry.prob+1)
+    temp$SST.remove       <- F
+    
+    temp$SST.remove[temp$sst.diff2>12] <- T
+    
+    output <- subset(temp,select=c(date,SST,SST.remove))
+  }
 }
